8a8ad2ba1961a24b5e3af0dccafb67f96106e67b
anassaffo <anassaffo@gmail.com>
2021-02-22 12:41:24 +0100

    changing how to handle NAs by replacing them with 0s. and experimenting with the count of Items

--- Anas's_report.R
+++ Anas's_report.R
@@ -1,14 +1,19 @@
 library(ggplot2)
 library(dplyr)
 library(tidyr)
+library(forcats)
 fao <- read.csv("FAO.csv")
 
 str(fao)
-# cleaning the data, by removing the NA values:
-cl_fao <- fao[complete.cases(fao),]
+# cleaning the data, by replacing NA values with 0:
+fao[is.na(fao)] <- 0
 
 # to visualize the change between years for the elements: feed and food
-cl_fao %>% select(Element, Y1961:Y2013) %>%
+fao %>% select(Element, Y1961:Y2013) %>%
     pivot_longer(., cols = c(Y1961:Y2013), names_to = "Var", values_to = "Val") %>%
     ggplot(aes(fill=Element, y=Val, x=Var)) + 
     geom_bar(position="stack", stat="identity")
+
+# counting the items, to know the importance of each one
+itm_count <- fct_count(fao$Item, sort = TRUE)
+
